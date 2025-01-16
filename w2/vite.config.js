79649a0101fafe4d55545b8de401c8db713eb75a
yuyeh <49071263+yuyeh1212@users.noreply.github.com>
2025-01-17 01:46:28 +0800

    修正路徑

--- w2/vite.config.js
+++ w2/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/React_hw/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/React_hw/w2/" : "/",
   plugins: [react()],
 });
